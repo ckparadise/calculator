9290e55050fbdcf9bd7b1ff96c21b3062a7dd0da
cksrms99 <cksrms99@gmail.com>
2023-10-25 22:34:23 +0900

    add main py  file

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+
+form_class = uic.loadUiType("main.ui")[0]
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUi()
+        
+    def initUI(self):
+        self.setWindowTitle('Calculator')
+        self.resize(256,256)
+        self.show()
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    sys.exit(app.exec_())
+    
+    #myWindow.show()
+    #app.exec_()
